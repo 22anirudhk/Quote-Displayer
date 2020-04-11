6dd74c7589a2dd330d248e19035a3b484b1cfde1
22anirudhk <45473342+22anirudhk@users.noreply.github.com>
2020-04-10 20:15:47 -0700

    Add more quotes.

--- popup/popup.js
+++ popup/popup.js
@@ -53,7 +53,7 @@ addQuote("Now in a dream, we create and perceive our world simultaneously and ou
 addQuote("Listen, if you’re going to perform inception you need imagination.", "Inception");
 addQuote("You need the simplest version of the idea in order for it to grow naturally in the subject’s mind.", "Inception");
 addQuote("In a dream you can cheat architecture into impossible shapes.", "Inception");
-addQuote("They come to be woken up. The dream has become their reality. Who are you to say otherwise.", "Inception");
+addQuote("They come to be woken up. The dream has become their reality. Who are you to say otherwise?", "Inception");
 addQuote("Whatever I do I can’t change this moment. I’m about to call out to them, they run away.", "Inception");
 addQuote("You’re waiting for a train, a train that will take you far away. You know where you hope this train will take you, but you don’t know for sure.", "Inception");
 
@@ -64,14 +64,29 @@ addQuote("We used to look up at the sky and wonder at our place in the stars, no
 addQuote("These moments when we dare to aim higher, to break barriers, to reach for the stars, to make the unknown known. We count these moments as our proudest achievements.", "Interstellar");
 addQuote("Do not go gentle into that good night; Old age should burn and rave at close of day. Rage, rage against the dying of the light.", "Interstellar");
 addQuote("We’re not meant to save the world, we’re meant to leave it.", "Interstellar");
-addQuote("Time is relative, okay? It can stretch and it can squeeze, but... it can’t run backwards. Just can’t. The only thing that can move across dimensions, like time, is gravity.", "Interstellar");
 addQuote("I’m not afraid of death. I’m an old physicist. I’m afraid of time.", "Interstellar");
 addQuote("Once you’re a parent, you’re the ghost of your children’s future.", "Interstellar");
 addQuote("Love is not something we invented. It’s observable, powerful. It has to mean something. Love is the one thing that transcends time and space.", "Interstellar");
 addQuote("I can promise you that, that yearning to be with other people is powerful. That emotion is the foundation, of what makes us human.", "Interstellar");
 addQuote("A machine doesn’t improvise well, because you can’t program the fear of death.", "Interstellar");
 
-// addQuote("", "Interstellar");
+
+addQuote("You either die a hero or live long enough to see yourself become the villain.", "The Dark Knight");
+addQuote("Sometimes the truth isn't good enough, sometimes people deserve more.", "The Dark Knight");
+addQuote("A hero can be anyone.", "The Dark Knight");
+addQuote("I fear dying in here, while my city burns, and there's no one there to save it.", "The Dark Knight");
+addQuote("He's the hero Gotham deserves, but not the one it needs right now.", "The Dark Knight");
+addQuote("He's not our hero. He's a silent guardian, a watchful protector. A dark knight.", "The Dark Knight")
+addQuote("You see, madness, as you know, is like gravity. All it takes is a little push!", "The Dark Knight");
+addQuote("Some men aren't looking for anything logical, like money. They can't be bought, bullied, reasoned, or negotiated with. Some men just want to watch the world burn.", "The Dark Knight");
+addQuote("If you're good at something, never do it for free.", "The Dark Knight");
+addQuote("Nobody panics when things go according to plan. Even if the plan is horrifying.", "The Dark Knight");
+addQuote("The night is darkest just before the dawn. And I promise you, the dawn is coming.", "The Dark Knight");
+addQuote("The night is darkest just before the dawn. And I promise you, the dawn is coming.", "The Dark Knight");
+
+
+
+
 
 changeWindow();
 
